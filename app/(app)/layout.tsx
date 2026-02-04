456e19e17b89447a2069856dd415d3573d8836dc
myanikaislam1920@gmail.com <myanikaislam1920@gmail.com>
2026-02-04 20:47:33 +0600

    added shadcn map

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,13 +1,16 @@
 import { AppHeader } from "@/components/app/layout/AppHeader"
+import { OnboardingGuard } from "@/components/app/layout/onboarding/OnboardingGuard"
 import { SanityLive } from "@/sanity/lib/live"
 import { ClerkProvider } from "@clerk/nextjs"
 
 function AppLayout({children} : {children: React.ReactNode}) {
   return (
     <ClerkProvider>
-      <AppHeader/>
+      <OnboardingGuard>
+          <AppHeader/>
       {children}
       <SanityLive/>
+      </OnboardingGuard>
     </ClerkProvider>
   )
 }
